32230449cc6282756bed92872a2457baade1108b
JaysonAbilar <abilarjayson18@gmail.com>
2016-12-29 23:25:14 +0800

    nawawala ung liststudent saka listsubject

--- src/pages/admin/admin-class-add/admin-class-add.ts
+++ src/pages/admin/admin-class-add/admin-class-add.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
 import { FIREBASE_PROVIDERS, defaultFirebase, AuthMethods, AuthProviders, firebaseAuthConfig } from 'angularfire2';
 import * as firebase from 'firebase';
-
 
 /*
   Generated class for the AdminClassAdd page.
@@ -26,13 +25,19 @@ export class AdminClassAddPage {
 	Section: ''
   };
 
+  currentAcademicYearObject: FirebaseObjectObservable<any>;
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public af: AngularFire) {
 
-  	this.classsList = this.af.database.list('/class');
+    this.currentAcademicYearObject = this.af.database.object('/current-academic-year');
+    this.currentAcademicYearObject.subscribe(snapshot => this.classs.StartYear = snapshot.Startyear); 
+    this.currentAcademicYearObject.subscribe(snapshot => this.classs.EndYear = snapshot.Endyear); 
+
+  	this.classsList = this.af.database.list('/academic-year/'+ this.classs.StartYear  + '-' + this.classs.EndYear + '/class');
   }
 
   addClass(StartYear, EndYear, Year, Section) {	   	
-  	  firebase.database().ref("/class/" + StartYear+"-"+EndYear+"_"+Year+"-"+Section).set({ 
+  	  firebase.database().ref('/academic-year/'+ this.classs.StartYear  + '-' + this.classs.EndYear + '/class/' + StartYear+"-"+EndYear+"_"+Year+"-"+Section).set({ 
   	  StartYear: StartYear,
       EndYear: EndYear,
       Year: Year,
--- src/pages/admin/admin-class-list/admin-class-list.ts
+++ src/pages/admin/admin-class-list/admin-class-list.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
 import { AdminHomePage } from '../admin-home/admin-home';
 import { AdminClassAddPage } from '../admin-class-add/admin-class-add';
 import { AdminClassUpdatePage } from '../admin-class-update/admin-class-update';
-import { AdminClassSubjectList2Page } from '../admin-class-subject-list-2/admin-class-subject-list-2';
-import { AdminClassStudentList2Page } from '../admin-class-student-list-2/admin-class-student-list-2';
-
 /*
   Generated class for the AdminClassList page.
 
@@ -20,9 +17,22 @@ import { AdminClassStudentList2Page } from '../admin-class-student-list-2/admin-
 export class AdminClassListPage {
 
   classsList: FirebaseListObservable<any>;
+  
+  currentAcademicYear = {
+    Startyear: '',
+    Endyear: ''
+  };
+
+  currentAcademicYearObject: FirebaseObjectObservable<any>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public af: AngularFire) {
-  	this.classsList = af.database.list('/class');
+
+    this.currentAcademicYearObject = this.af.database.object('/current-academic-year');
+    this.currentAcademicYearObject.subscribe(snapshot => this.currentAcademicYear.Startyear = snapshot.Startyear); 
+    this.currentAcademicYearObject.subscribe(snapshot => this.currentAcademicYear.Endyear = snapshot.Endyear); 
+
+    this.classsList = this.af.database.list('/class');
+
   }
   
   goToAdminClassAdd(){
@@ -44,26 +54,6 @@ export class AdminClassListPage {
 
   deleteClass(classs) {
    this.classsList.remove(classs);
-  }
-
-  listSubject(classs){
-    this.navCtrl.push(AdminClassSubjectList2Page, {
-      key: classs.$key,
-      StartYear: classs.StartYear,
-      EndYear: classs.EndYear,
-      Year: classs.Year,
-      Section: classs.Section
-    });
-  }
-
-  listStudents(classs){
-    this.navCtrl.push(AdminClassStudentList2Page, {
-      key: classs.$key,
-      StartYear: classs.StartYear,
-      EndYear: classs.EndYear,
-      Year: classs.Year,
-      Section: classs.Section
-    });
   }
 
   ionViewDidLoad() {
