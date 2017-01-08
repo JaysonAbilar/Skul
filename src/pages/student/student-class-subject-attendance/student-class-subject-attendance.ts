237536ac6f51e167e2465435200d7cb71a0efd48
JaysonAbilar <abilarjayson18@gmail.com>
2017-01-09 01:54:01 +0800

    students viewing of attendace and profile

--- src/pages/student/student-class-subject-attendance/student-class-subject-attendance.ts
+++ src/pages/student/student-class-subject-attendance/student-class-subject-attendance.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
 
 /*
   Generated class for the StudentClassSubjectAttendance page.
@@ -13,7 +14,28 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class StudentClassSubjectAttendancePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  public Username:any;
+  public ClassId:any;
+  public Startyear:any;
+  public Endyear:any;
+  public SubjectCode:any;
+ 
+  attendanceDateList: FirebaseListObservable<any>;
+  attendanceDate = {
+  	Date:''
+  }
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public af: AngularFire) {
+
+  	 this.Username = this.navParams.get('Username');
+  	 this.ClassId = this.navParams.get('ClassId');
+  	 this.Startyear = this.navParams.get('Startyear');
+  	 this.Endyear = this.navParams.get('Endyear');
+  	 this.SubjectCode = this.navParams.get('Subjectcode');
+
+  	 this.attendanceDateList = this.af.database.list('/academic-year/'+ this.Startyear  + '-' + this.Endyear  + '/class-subject/' + this.ClassId + '/' + this.SubjectCode + '/student-attendance/' + this.Username);
+
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad StudentClassSubjectAttendancePage');
--- src/pages/student/student-profile/student-profile.ts
+++ src/pages/student/student-profile/student-profile.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { StudentHomePage } from '../student-home/student-home';
+import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
 /*
   Generated class for the StudentProfile page.
 
@@ -13,7 +14,36 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class StudentProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  Username:'';
+  studentObject: FirebaseObjectObservable<any>;
+
+  student = {
+    Username: '',
+	Password: '',
+	Firstname: '',
+	Middlename:'',
+	Lastname: '',
+	Age: '',
+	Gender: '',
+	Email: '',
+	Contactnumber: ''
+  };
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public af: AngularFire) {
+  	this.Username =  this.navParams.data.Username;
+
+  	this.studentObject = af.database.object('/student/' + this.Username);
+    
+    this.studentObject.subscribe(snapshot => this.student.Username = snapshot.$key); 
+    this.studentObject.subscribe(snapshot => this.student.Password = snapshot.Password);
+    this.studentObject.subscribe(snapshot => this.student.Firstname = snapshot.Firstname);
+    this.studentObject.subscribe(snapshot => this.student.Middlename = snapshot.Middlename);
+    this.studentObject.subscribe(snapshot => this.student.Lastname = snapshot.Lastname);
+    this.studentObject.subscribe(snapshot => this.student.Age = snapshot.Age);
+    this.studentObject.subscribe(snapshot => this.student.Gender = snapshot.Gender);
+    this.studentObject.subscribe(snapshot => this.student.Email = snapshot.Email);
+    this.studentObject.subscribe(snapshot => this.student.Contactnumber = snapshot.Contactnumber);
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad StudentProfilePage');
